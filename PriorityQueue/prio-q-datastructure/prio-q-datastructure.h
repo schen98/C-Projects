8d30868fc15c1f9eb57cba46d32a5e7e7885574f
schen98 <70492219+schen98@users.noreply.github.com>
2021-07-05 17:40:24 -0400

    Update prio-q-datastructure.h

--- PriorityQueue/prio-q-datastructure/prio-q-datastructure.h
+++ PriorityQueue/prio-q-datastructure/prio-q-datastructure.h
@@ -8,8 +8,8 @@
  *
  * header file consist of a node abstract data type
  * called Prio_que with members, int prio for priority,
- * char* element to point to string, and a struct
- * node* next to point back to the struct.
+ * char* element to point to string, and a struct node* 
+ * next to point back to the struct.
  */
 
 typedef struct node {
