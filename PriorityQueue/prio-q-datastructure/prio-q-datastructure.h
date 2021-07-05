b71020bb6b0cff67e85fe93e2a081626b0014f82
schen98 <70492219+schen98@users.noreply.github.com>
2021-07-05 17:36:55 -0400

    Add files via upload

---
+++ PriorityQueue/prio-q-datastructure/prio-q-datastructure.h
@@ -0,0 +1,19 @@
+/* Name: Simon Chen
+ * TerpConnect login ID: schen98
+ * Univserty ID number: 117009781
+ * Discussion section: 0104
+ *
+ * I pledge on my honor that I have not given or received any
+ * unauthorized assistance on this assignments or examination.
+ *
+ * header file consist of a node abstract data type
+ * called Prio_que with members, int prio for priority,
+ * char* element to point to string, and a struct
+ * node* next to point back to the struct.
+ */
+
+typedef struct node {
+  int prio;
+  char *element;
+  struct node* next;
+} Prio_que;
